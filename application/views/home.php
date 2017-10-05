443a668f72e01776f7d9eab789702213d0c0ddd8
antony13 <nanosa@uni.coventry.ac.uk>
2017-10-05 21:49:56 +0000

    Ajax for uploading the image

--- application/views/home.php
+++ application/views/home.php
@@ -11,10 +11,35 @@
 </head>
 <body>
 <div class="ui left icon input">
-<?=form_open_multipart('base/upload_file');?>
-  <input type="file" name='the_file'>
-<input type='submit' />
+<?php $attributes = array('id' => 'file_form'); ?>
+<?=form_open_multipart('base/upload_file',$attributes);?>
+  <input type="file" name='the_file' id='file_id'>
+<input type='submit' id='submit_button' />
 <?=form_close();?>
 </div>
+<script type='text/javascript'>
+$( document ).ready(function() {
+        $('#submit_button').on('click',function(e) {
+                e.preventDefault();
+		if (confirm('Are you sure you want to submit this image?'))
+                {
+                var file_data = new FormData($('#file_form')[0]);
+		$.ajax({
+			type:'POST',
+			url:"<?=base_url('index.php/base/upload_file');?>",
+			processData: false,
+			contentType: false,
+			async: true,
+			cache: false,
+			data : file_data,
+			success: function(response){
+				alert(response);
+			}
+		});//end of ajax
+		}//end if
+	});
+});//end of document ready
+
+</script>
 </body>
 </html>
