016f64d2409da0e8fb355e11801ec74dd6d9d5d5
antony13 <nanosa@uni.coventry.ac.uk>
2017-10-05 19:21:46 +0000

    Prepared the front end

---
+++ application/views/home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="<?=base_url('front_end/css/');?>semantic.min.css">
+<script
+  src="https://code.jquery.com/jquery-3.2.1.min.js"
+  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+  crossorigin="anonymous"></script>
+<script src="<?=base_url('front_end/js/');?>semantic.min.js"></script>
+</head>
+<body>
+
+</body>
+</html>
